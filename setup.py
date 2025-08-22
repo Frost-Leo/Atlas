149d50e7389160fcaf41e08e1714b2f68b99bf00
Frost Leo <FrostLeo.Dev@gmail.com>
2025-08-22 14:10:54 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# @FileName      : setup.py
+# @Created Time  : 2025/8/22 14:00
+# @Author        : FrostLeo
+# @Email         : FrostLeo.Dev@gmail.com
+# -----------------------------------------------------------------------------
+
+"""
+**Atlas package setup programming**
+
+This module is responsible for handling the installation of Atlas。
+"""
+
+from setuptools import setup, find_packages
+
+
+if __name__ == '__main__':
+    setup()
