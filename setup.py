e1e5d0b59e32de278f6b1e5a3166f1d0cc3ffa0d
Frost Leo <FrostLeo.Dev@gmail.com>
2025-09-10 17:53:57 +0800

    feat: Add Python package configuration (pyproject.toml, setup.py)
    - Configure build system and dependencies
    - Add development tools (pytest, mypy, coverage)
    - Create src/atlas package structure

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+setup
+
+Atlas Installer.
+
+- Author   : FrostLeo <frostleo.dev@gmail.com>
+- Created  : 2025/9/10 
+- Modified : 2025/9/10
+- License  : GPL-3.0
+"""
+
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    setup()
---
+++ src/atlas/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+__init__.py
+
+Atlas packages
+
+- Author   : FrostLeo <frostleo.dev@gmail.com>
+- Created  : 2025/9/10 
+- Modified : 2025/9/10
+- License  : GPL-3.0
+"""
+
+__version__ = "0.1.0"
